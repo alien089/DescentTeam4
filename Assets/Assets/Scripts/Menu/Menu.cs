525fd31364bae14c99b3c836a36299b83e28e67b
Alien089 <gabri.moratelli@gmail.com>
2023-03-26 17:51:40 +0200

    added main menu

--- Assets/Assets/Scripts/Menu/Menu.cs
+++ Assets/Assets/Scripts/Menu/Menu.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public AudioMixer AudioMixer;
+
     public void Play()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void EnterOvering(Text testo)
+    {
+        testo.color = Color.yellow;
+    }
+    
+    public void ExitOvering(Text testo)
+    {
+        testo.color = Color.gray;
+    }
+
+    public void SetMusicVolume(Slider volume)
+    {
+        AudioMixer.SetFloat("Master", volume.value);
     }
 }
