277db7fd0037ddddfb07b45794bdee4b8c12f64b
Alien089 <gabri.moratelli@gmail.com>
2023-03-29 18:01:35 +0200

    fix bug singleton

--- Assets/Assets/Scripts/UIStuff.cs
+++ Assets/Assets/Scripts/UIStuff.cs
@@ -21,12 +21,6 @@ public class UIStuff : Singleton<UIStuff>
 
     public Text Notification;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    
     // Update is called once per frame
     void Update()
     {
